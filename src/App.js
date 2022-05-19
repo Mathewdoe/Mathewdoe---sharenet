76641d59f1bb8f78f51cbc4eef95e0741910f132
Mathewdoe <thandazani.matthew@gmail.com>
2022-05-19 16:07:19 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {Card, Button,DropdownButton,Dropdown} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {useState,useEffect} from 'react';
+
+// const LightTheme = {
+//   pageBackground:'white',
+//   titleColor:'#dc658b',
+//   tagLineColor:'black'
+// }
+
+  const baseURL='http://localhost:3004/info';
 
 function App() {
+
+  const [workShop , setWorkShop] = useState([]);
+
+
+  useEffect(() => {
+    getWorkshop();
+  }, []);
+
+  // Fetch data from Json Server
+  const getWorkshop = async () => {
+    await axios.get(baseURL).then((res) => setWorkShop(res.data));
+  };
+
+  
+  
+// console.log(workShop[0].venue)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      
+    {workShop.map((x, index)=>{
+      return(
+        <Card style={{ width: '15rem', display: 'inline-block', margin:'5px'}} key={index}>
+        <Card.Body>
+         <Card.Text>
+          <span style={{fontSize: "2rem"}}>{x.venue}</span>
+          <br />
+          <span style={{fontSize: "1.5rem"}}>{x.date}</span>
+         </Card.Text>
+         <DropdownButton id="dropdown-basic-button" title={x.availability}>   
+       <Dropdown.Item >{x.seats}</Dropdown.Item>
+      </DropdownButton>
+       </Card.Body>
+     </Card>
+      )
+    })}
+    
     </div>
+    
+   
+    
   );
 }
 
